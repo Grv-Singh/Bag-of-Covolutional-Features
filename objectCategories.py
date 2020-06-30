90e1ec4b01866854c8ff2ef0240bc7adf40cf4b9
Gaurav Singh <acad.grv97@gmail.com>
2020-06-30 19:39:58 +0530

    Update objectCategories.py

--- objectCategories.py
+++ objectCategories.py
@@ -112,6 +112,7 @@ Y = [i[1] for i in train]
 test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
 test_y = [i[1] for i in test]
 
+#training here
 model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}), 
     snapshot_step=10, show_metric=True, run_id=MODEL_NAME)
 
